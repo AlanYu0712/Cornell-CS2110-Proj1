e31de652f6c6414229d36fc6b8263dfd956b07e0
AlanYu0712 <alanyu0712@gmail.com>
2019-06-30 23:36:46 -0400

    completing comments and final check

--- src/project1/Exercises.java
+++ src/project1/Exercises.java
@@ -4,21 +4,29 @@ import java.util.Arrays;
 
 public class Exercises {
 	
+	
 	/**
 	 * Return true if s is the same backwards and forwards.
 	 * @param s inputed String
 	 * @return whether the string is the same backwards and forwards.
 	 */
 	public static boolean isPalindrome(String s) {
+		
+		//setting precondition
 		s=s.toLowerCase();
 
-		for(int i=0; i<s.length(); i++) {
-			if (s.charAt(i)!= s.charAt(s.length()-1-i)) {
+		for(int i = 0; i < s.length(); i++) {
+			
+			if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+				
 				return false;
 			}
 		}
+		
 		return true;
 	}
+	
+	
 	
 	/**
 	 * Return a copy of s with all whitespace and punctuation removed.
@@ -27,11 +35,15 @@ public class Exercises {
 	 */
 	public static String normalize(String s) {
 		
+		// remove all whitespace
 		s = s.replaceAll("\\s", "");
+		//remove all punctuation
 		s = s.replaceAll("\\W", "");
 		
 		return s;
 	}
+	
+	
 	
 	/**
 	 * find the integer median of a integer array
@@ -39,22 +51,29 @@ public class Exercises {
 	 * @return the integer median of this inputed array
 	 */
 	public static int median(int[ ] values) {
+		
+		//sort the array from small value to big
 		Arrays.sort(values);
-	
+		
+		//setting variable to pass the median
 		int result = 0;
 		
-		
+		//in cases where the length of the array is odd
 		if(values.length%2 == 1) {
 			
-			result = values[(values.length-1)/2];
-			
-		} else if(values.length%2 == 0) {
-			result = (values[values.length/2]);
+			result = values[(values.length-1) / 2];			
 		}
-		
+		// in cases where the length of the array is even
+		else if(values.length%2 == 0) {
+			
+			//return only integer
+			result = (values[values.length/2]);
+		}	
 		
 		return result;
 	}
+	
+	
 	
 	/**
 	 * counting the zeros in an array
@@ -62,14 +81,21 @@ public class Exercises {
 	 * @return the number of zeros it has in that array
 	 */
 	public static int numZeros(int[ ] values) {
+		
 		int x = 0;
-		for (int i= 0; i<values.length; i++) {
-			if (values[i]==0) {
-				x= x+1;
+		
+		for (int i = 0; i < values.length; i++) {
+			
+			if (values[i] == 0) {
+				
+				x = x+1;
 			}
 		}
+		
 		return x;
 	}
+	
+	
 	
 	/**
 	 * Find the mean in an integer array
@@ -77,13 +103,22 @@ public class Exercises {
 	 * @return the the value of the mean of the inputed integer array
 	 */
 	public static double mean(int[ ] values) {
-		double x=0;
-		for (int i=0; i<values.length; i++) {
-			x= x+ values[i];
+		
+		double x = 0;
+		
+		//adding all values in the array
+		for (int i = 0; i < values.length; i++) {
+			
+			x = x + values[i];
 		}
-		x= x/values.length;
+		
+		//Divide sum by array length
+		x = x/values.length;
+		
 		return x;
 	}
+	
+	
 	
 	/**
 	 * Find the whether the diagonal value of a 2D array is the same
@@ -93,36 +128,36 @@ public class Exercises {
 	 * values[i].length== values.length).
 	 */
 	public static boolean hasConstDiagonal(int[ ][ ] values) {
+		
+		// asserting the 2D array is has the same number of columns and rows
 		assert values.length == values[0].length;
 		
+		//initialize boolean to return
 		boolean pass = false;
 		
+		// initializing variable to check diagonal from left to right 
 		int Left2Right = values[0][0];
-		
+		// initializing variable to check diagonal from right to left
 		int Right2Left = values[values.length-1][0];
 		
-		for (int i=0; i<values[0].length; i++) {
+		//checking diagonal from left to right and right to left simultaneously
+		for (int i = 0; i < values[0].length; i++) {
 			
-			if(Left2Right == values[i][i] && Right2Left == values[values.length-(1+i)][i]) {
+			if(Left2Right == values[i][i] && Right2Left == values[values.length - (1+i)][i]) {
+				
 				pass = true;
 			} else {
+				
 				pass = false;
 				break;
-			}
-		
+			}		
 		}
 		
 		
 		return pass;
-		
-
 	}
 	
-	public static void main(String[] args) {
-		int[] test = {1,2,3,4,5};
-		System.out.println(test[-1]);
-		
-	}
+
 	
 	
 
--- src/project1/ExercisesTest.java
+++ src/project1/ExercisesTest.java
@@ -8,65 +8,109 @@ class ExercisesTest {
 	
 
 	@Test
-	void test_Palindrome() {
+	/**
+	 * Testing the first method in Exercises: isPalindrome(String s)
+	 */
+	void test_Palindrome() {		
 		
-		
+		// create instance of object Exercises
 		Exercises e = new Exercises();
 		
-		assertEquals(true, e.isPalindrome("Radar"));
-
-		
+		//checking does outcome of method satisfies expectation
+		assertEquals(true, e.isPalindrome("Radar"));		
 	}
+
 	
 	
 	@Test
+	/**
+	 * Testing the second method in Exercises: normalize(String s)
+	 */
 	void test_Normalize() {
+		
+		//create instance of object Exercises
 		Exercises e = new Exercises();
 		
+		//checking does outcome of method satisfies expectation
 		assertEquals("Java", e.normalize("J a:;v. a,"));
 	}
 	
 	
+	
 	@Test
+	/**
+	 * Testing the third method in Exercises: median(int[ ] values)
+	 */
 	void test_Median() {
+		
+		//create instance of object Exercises
 		Exercises e = new Exercises();
 		
+		//create an array with a length that is odd
 		int[] checker1 = {3,5,1,2,4};
-		
+		// checking does outcome of method satisfies expectation
 		assertEquals(3, e.median(checker1));
 		
+		//create an array with a length that is even
 		int[] checker2 = {8,4,2,6};
-		
+		//checking does outcome of method satisfies expectation
 		assertEquals(6, e.median(checker2));
 	}
 	
 	
+	
 	@Test
+	/**
+	 * Testing the fourth method in Exercises: numZeros(int[ ] values)
+	 */
 	void test_num0s() {
+		
+		//create instance of object Exercises
 		Exercises e = new Exercises();
 		
+		// create an array for testing
 		int[] checker = {2,0,1,9,6,3,0};
 		
-		assertEquals(2, e.numZeros(checker));
-		
+		//checking does outcome of method satisfies expectation
+		assertEquals(2, e.numZeros(checker));		
 	}
 	
 	
+	
 	@Test
+	/**
+	 * Testing the fifth method in Exercises: mean(int[ ] values)
+	 */
 	void test_mean() {
+		
+		//create instance of object Exercises
 		Exercises e = new Exercises();
 		
-		int[] checker = {1,3,5,7,9,2,4,6,8,10};		
+		//create an array for testing
+		int[] checker = {1,3,5,7,9,2,4,6,8,10};
+		
+		//checking does outcome of method satisfies expectation
+		assertEquals(5.5, e.mean(checker));
 	}
 	
+	
+	
 	@Test
+	/**
+	 * testing the sixth and last method in Exercises: hasConstDiagonal(int[ ][ ] values)
+	 */
 	void test_Diag() {
 		
+		//create instance of object Exercises
 		Exercises e = new Exercises();
+		
+		//create a 2D array for testing
 		int[][] checker = { {1,2,1},{3,1,2},{1,3,1}};
 		
-		assertEquals(true, e.hasConstDiagonal(checker));
-		
+		//checking does outcome of method satisfies expectation
+		assertEquals(true, e.hasConstDiagonal(checker));	
 	}
-
+	
+	
+	
 }
